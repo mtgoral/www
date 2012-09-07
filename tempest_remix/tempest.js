4e9f2135632f91bb3bd18de2a60dd52321bde933
libra guest <mtgoral@gmail.com>
2012-09-06 23:44:21 -0400

    i am fucking awesome

--- tempest_remix/tempest.js
+++ tempest_remix/tempest.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
         function goto(page) {
             var target = page;
             $('.container').fadeOut(1000);
+            $('#bg').fadeOut(1000);
 			setTimeout(function() { window.location.href = target; }, 1000);
 		}
 
@@ -19,23 +20,17 @@ $(document).ready(function() {
 
 		function render() {
             $(".header").append(
-            '<a href="#" id="home" onclick="javascript:home();return false;" style="position:relative;bottom:0;left:0;"><img src="logo.png"></a>' +
+            '<a href="#" id="home" onclick="javascript:goto(\'../index.html\');return false;" style="position:relative;bottom:0;left:0;"><img src="http://www.mattgoral.com/tempest_remix/img/logo.png"></a>' +
             '<a href="#" id="whois" onclick="javascript:goto("whois");return false;" style="position:relative;bottom:5px;left:-60px;">who is tempest</a>' +
             '<a href="#" id="shows" onclick="javascript:shows();return false;" style="position:relative;bottom:5px;left:-50px;">shows</a>' +
             '<a href="#" id="music" onclick="javascript:music();return false;" style="position:relative;bottom:5px;left:-40px;">music</a>' +
             '<a href="#" id="photos" onclick="javascript:goto(\'photos/index.html\');return false;" style="position:relative;bottom:5px;left:-30px;">photos</a>' +
             '<a href="#" id="videos" onclick="javascript:videos();return false;" style="position:relative;bottom:5px;left:-20px;">videos</a>' +
-            '<a href="#" id="facebook" onclick="javascript:shows();return false;" style="position:absolute;bottom:5px;left:1048px;width:24px;height:28px;"><img src="facebook.png"></a>' +
-            '<a href="#" id="twitter" onclick="javascript:shows();return false;" style="position:absolute;bottom:5px;left:1080px;width:24px;height:30px;"><img src="twitter.png"></a>' +
-            '<a href="#" id="youtube" onclick="javascript:shows();return false;" style="position:absolute;bottom:5px;left:1115px;width:24px;height:30px;"><img src="youtube.png"></a>' +
-            '<a href="#" id="soundcloud" onclick="javascript:shows();return false;" style="position:absolute;bottom:5px;left:1150px;width:24px;height:30px;"><img src="soundcloud.png"></a>'
+            '<a href="#" id="facebook" onclick="javascript:shows();return false;" style="position:relative;bottom:22px;left:-221px;width:24px;height:28px;"><img src="http://www.mattgoral.com/tempest_remix/img/facebook.png"></a>' +
+            '<a href="#" id="twitter" onclick="javascript:shows();return false;" style="position:relative;bottom:19px;left:-214px;width:24px;height:30px;"><img src="http://www.mattgoral.com/tempest_remix/img/twitter.png"></a>' +
+            '<a href="#" id="youtube" onclick="javascript:shows();return false;" style="position:relative;bottom:19px;left:-210px;width:24px;height:30px;"><img src="http://www.mattgoral.com/tempest_remix/img/youtube.png"></a>' +
+            '<a href="#" id="soundcloud" onclick="javascript:shows();return false;" style="position:relative;bottom:20px;left:-205px;width:24px;height:30px;"><img src="http://www.mattgoral.com/tempest_remix/img/soundcloud.png"></a>'
             );
-            
-            
-            
-            
-            
-			$('#bg').fadeIn(1000);
 			setTimeout(function() { $('#home').fadeIn(1000); }, 200);
 			setTimeout(function() { $('#whois').fadeIn(1000); }, 300);
 			setTimeout(function() { $('#shows').fadeIn(1000); }, 400);
