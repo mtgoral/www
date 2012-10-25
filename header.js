38ad0f96efc31fde7d415589473e1b72e326cfba
libra guest <mtgoral@gmail.com>
2012-10-25 18:34:06 -0400

    blah

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  $("#header").append('<a href="index.html"><h1>matt goral</h1></a><br><a class="headerlink" href="projects.html">projects</a><a class="headerlink" href="resume.html">resume</a><a class="headerlink" href="about.html">about</a><a class="headerlink" href="contact.html">contact</a>');
+  $("#header").append('<a href="index.html"><h1>matt goral</h1></a><br><a class="headerlink" href="work.html">work</a><a class="headerlink" href="resume.html">resume</a><a class="headerlink" href="about.html">about</a><a class="headerlink" href="contact.html">contact</a>');
   $('#header').css('height', $(window).height());
 });
